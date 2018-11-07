89516cd7e3bcbab327032dfc15eb2d61d9742081
Yu-Hsien Chou <slps970093@gmail.com>
2018-11-07 23:26:30 +0800

    clean code

--- app/src/main/java/com/opendata/yu_hsienchou/taiwanair/MainActivity.java
+++ app/src/main/java/com/opendata/yu_hsienchou/taiwanair/MainActivity.java
@@ -1,13 +1,16 @@
 package com.opendata.yu_hsienchou.taiwanair;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,7 +21,10 @@ import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
@@ -32,44 +38,56 @@ import permissions.dispatcher.PermissionRequest;
 import permissions.dispatcher.RuntimePermissions;
 
 @RuntimePermissions
-public class MainActivity extends AppCompatActivity implements LocationListener,GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener {
+public class MainActivity extends AppCompatActivity {
     private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
     private Location currentLocation;
-    private GoogleApiClient googleApiClient;
     private FusedLocationProviderClient fusedLocationProviderClient;
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MainActivityPermissionsDispatcher.GPSLocationWithPermissionCheck(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         AirBoxAsyncTask airBoxAsyncTask = new AirBoxAsyncTask();
-        googleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
         try{
             airBoxAsyncTask.execute();
             // 取得空氣資料
-            if(airBoxAsyncTask.getStatusCode() == 200){
-                ArrayList<AirDataModel> airModel = airBoxAsyncTask.getModel();
+        //    ArrayList<AirDataModel> airModel = airBoxAsyncTask.getModel();
+            setLocationRequest();
+            getLocationInfo();
 
-                locationRequest = new LocationRequest()
-                        .setInterval(1000)  // 設定讀取位置資訊的間隔時間為一秒（1000ms）
-                        .setFastestInterval(1000)   // 設定讀取位置資訊最快的間隔時間為一秒（1000ms）
-                        .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);   // 設定優先讀取高精確度的位置資訊（GPS）
-                fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-                fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        if(location != null){
-                            Toast.makeText(MainActivity.this,"gps"+location.getLongitude(),Toast.LENGTH_LONG).show();
-                        }
+            locationCallback = new LocationCallback(){
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    super.onLocationResult(locationResult);
+                    for(Location location : locationResult.getLocations()){
+                        Toast.makeText(MainActivity.this,"Lat: "+location.getLatitude()+" Log: "+location.getLongitude(),Toast.LENGTH_SHORT).show();
                     }
-                });
-            }
+                }
+
+                @Override
+                public void onLocationAvailability(LocationAvailability locationAvailability) {
+                    super.onLocationAvailability(locationAvailability);
+                }
+            };
+
+
         }catch (Exception e){
             e.printStackTrace();
+        }
+    }
+
+    private void getLocationInfo(){
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if( location != null ){
+                        Toast.makeText(MainActivity.this,"Lat: "+location.getLatitude()+" Log: "+location.getLongitude(),Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
         }
     }
     @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
@@ -112,60 +130,12 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
     }
 
     /**
-     * 定位狀態改變
-     * @param provider
-     * @param status
-     * @param extras
+     * 設定 LocationRequest
      */
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    /**
-     * 當GPS或網路定位功能開啟
-     * @param provider
-     */
-    @Override
-    public void onProviderEnabled(String provider) {
-        Toast.makeText(MainActivity.this,"開始定位！",Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * 當GPS或網路定位功能關閉時
-     * @param provider
-     */
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
-
-    /**
-     * 當地點改變
-     * @// TODO: 2018/11/7 https://www.youtube.com/watch?v=P0dYZcE8510&t=657s 
-     * @param location
-     */
-    @Override
-    public void onLocationChanged(Location location) {
-        currentLocation = location;
-        if(currentLocation != null){
-            Toast.makeText(MainActivity.this,"gps"+currentLocation.getLongitude(),Toast.LENGTH_LONG).show();
-        }
-    }
-
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        LocationServices.getFusedLocationProviderClient(this);
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+    private void setLocationRequest(){
+        locationRequest = new LocationRequest()
+                .setInterval(1000)  // 設定讀取位置資訊的間隔時間為一秒（1000ms）
+                .setFastestInterval(1000)   // 設定讀取位置資訊最快的間隔時間為一秒（1000ms）
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY) ;  // 設定優先讀取高精確度的位置資訊（GPS）
     }
 }
